5a9ae3cc382400591fb3d53570de45e6ac1cb471
Ghostdevol <karmastudiosbeatz@gmail.com>
2026-09-21 14:42:22 -0500

    USB launcher finds CLI by wildcard

--- FanTom-LS USB.bat
+++ FanTom-LS USB.bat
@@ -1,4 +1,5 @@
 @echo off
-set "PCMHAMMER_PATH=%~dp0tools\pcmh\PcmHammerCLI.exe"
+rem Find the CLI however the release zip named it (PcmHammerCLI.exe, pcmhammer-cli.exe, ...)
+for %%F in ("%~dp0tools\pcmh\*hammer*cli*.exe") do set "PCMHAMMER_PATH=%%~fF"
 set "XDFDEFINITIONS_PATH=%~dp0tools\xdf"
 start "" "%~dp0fantom-ls.exe"
